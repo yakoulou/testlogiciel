092f90228bcc1dd7098181167bcc4adc6e411d90
yakoulou <habib.benmansour281802@gmail.com>
2025-04-11 10:40:03 +0200

    first commit

---
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -0,0 +1,15 @@
+package com.example.demo.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.*;
+
+@SpringBootTest
+public class VoitureTest {
+
+    @Test
+    void creerVoiture(){
+
+    }
+
+}
