b7af43cf461287bd66078a4eb066ffa794e39155
yakoulou <habib.benmansour281802@gmail.com>
2025-05-21 11:01:31 +0200

    versiontp4

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -5,6 +5,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.*;
 import org.junit.jupiter.api.Assertions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
 @SpringBootTest
 public class VoitureTest {
 
@@ -12,6 +16,10 @@ public class VoitureTest {
     void creerVoiture() {
     	Voiture v1=new Voiture("clio4",3000);
         Assert.isTrue(v1.getMarque() == "clio3", "La marque ne correspond pas");
+        v1.setMarque("Peugeot");
+        v1.setPrix(25000);
+        assertEquals("Peugeot", v1.getMarque());
+        assertEquals(25000, v1.getPrix());
 
     }
     	
--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -26,13 +26,17 @@ public class StatistiqueTests {
         Voiture v2 = mock(Voiture.class);
         when(v1.getPrix()).thenReturn(2000);// si jamais dans la suite du programme tu a besoin de faire un getprix() tu vas me mettre 100
         when(v2.    getPrix()).thenReturn(3000);// Quelle comportement le mock doit avoir !
-
+            s1.ajouter(v1);
+            s1.ajouter(v2);
         Echantillon e1 = s1.prixMoyen();
+
        // when(s1.prixMoyen()).thenReturn(new Echantillon(2, 2500));
         Assert.isTrue(s1.prixMoyen().getNombreDeVoitures() == 2, "Le nombre de voitures n'est pas correct");
         Assert.isTrue(s1.prixMoyen().getPrixMoyen() == 2900, "Le prix moyen n'est pas correct");
+            assertEquals(2, e1.getNombreDeVoitures());
+            assertEquals(3500, e1.getPrixMoyen());
 
-        assertEquals(e1.getPrixMoyen(),0);
+            assertEquals(e1.getPrixMoyen(),0);
          } catch (ArithmeticException e) {
             System.out.println("pas de voiture");
         }
